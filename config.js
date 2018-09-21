c50037995165bd1c368301365d2d4069b8c0b565
damianjurek1985 <damianjurek85@gmail.com>
2018-09-21 23:44:39 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    
+    "database": "mongodb://root:Respecta19850409@ds137730.mlab.com:37730/simplyapp",
+    "port": process.env.PORT || 3000,
+    "secretKey": "Respecta1985"
+}
